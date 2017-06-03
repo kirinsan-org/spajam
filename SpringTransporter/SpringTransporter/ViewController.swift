51dafba019820f853466db26c414cae0805c4f2d
星野恵瑠 <shonetesmith@gmail.com>
2017-06-03 18:15:57 +0900

    init Xcode project

---
+++ SpringTransporter/SpringTransporter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpringTransporter
+//
+//  Created by 史翔新 on 2017/06/03.
+//  Copyright © 2017年 kirinsan.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
