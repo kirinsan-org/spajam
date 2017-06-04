c728195bc9eac3a233f506c2ac36634e3c104147
Jun Tanaka <junpluse@gmail.com>
2017-06-04 11:09:31 +0900

    Implement SensorScanner

--- SpringTransporter/SpringTransporter/AppDelegate.swift
+++ SpringTransporter/SpringTransporter/AppDelegate.swift
@@ -16,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FIRApp.configure()
-		// Override point for customization after application launch.
+
+        SensorScanner.shared.start()
+
 		return true
 	}
 
---
+++ SpringTransporter/SpringTransporter/SeasonCharacter.swift
@@ -0,0 +1,28 @@
+//
+//  SeasonCharacter.swift
+//  SpringTransporter
+//
+//  Created by Jun Tanaka on 2017/06/04.
+//  Copyright © 2017 kirinsan.org. All rights reserved.
+//
+
+import Foundation
+
+enum SeasonType: String {
+    case spring = "spring"
+    case summer = "summer"
+    case autumn = "autumn"
+    case winter = "winter"
+}
+
+enum SeasonCharacterState: Int {
+    case born = 0
+    case live = 1
+    case dead = 2
+}
+
+struct SeasonCharacter {
+    var type: SeasonType
+    var state: SeasonCharacterState
+    var pain: Int
+}
